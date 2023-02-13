30ab996ac25e3f4b8fbec944c4369dfbed446bf7
ngoctuds73 <123917622+ngoctuds73@users.noreply.github.com>
2023-02-13 10:49:50 +0700

    Update cat.py

--- hierarchical_inheritance/project/cat.py
+++ hierarchical_inheritance/project/cat.py
@@ -1,5 +1,9 @@
 from hierarchical_inheritance.project.animal import Animal
 
 class Cat(Animal):
+    
+    def __init__(self):
+        super().__init__()
+        
     def meow(self):
         return "meowing..."
